f61115aaa68813504c04b5c53bd1e723d0db6fc4
bileamScheuvens <benedictbileam@gmx.de>
2025-11-17 11:31:53 +0100

    allow ctrl backspace for bash

--- modules/bash.nix
+++ modules/bash.nix
@@ -15,7 +15,10 @@
       poetry install --no-root && fix-python --venv $(poetry env info -p) --libs /etc/nixos/modules/libs.nix
     }
 
+    # ignore certain endings for nvim
     complete -f -X "*.@(bib|pdf|png|jpg)" nvim
 
+    # ctrl+backspace to delete word
+    bind '"\C-H": backward-kill-word'
   '';
 }

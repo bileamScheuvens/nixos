35e16e28d9e0145faf9deb66dc2bb808bf548c21
bileamScheuvens <benedictbileam@gmx.de>
2025-12-19 23:37:57 +0100

    improve system copy & paste

--- nixvim-config/config/keymap.nix
+++ nixvim-config/config/keymap.nix
@@ -38,6 +38,43 @@
     key = "<M-k>";
     action = "<cmd>cprev<CR>";
   }
+  # system copy & paste with leader
+  {
+    mode = "v";
+    key = "<leader>y";
+    action = "\"+y";
+  }
+  {
+    mode = "n";
+    key = "<leader>y";
+    action = "\"+y";
+  }
+  {
+    mode = "n";
+    key = "<leader>Y";
+    action = "\"+yg_";
+  }
+  {
+    mode = "v";
+    key = "<leader>p";
+    action = "\"+p";
+  }
+  {
+    mode = "v";
+    key = "<leader>P";
+    action = "\"+P";
+  }
+  {
+    mode = "n";
+    key = "<leader>p";
+    action = "\"+p";
+  }
+  {
+    mode = "v";
+    key = "<leader>P";
+    action = "\"+P";
+  }
+
   # obsidian new note and insert template
   # {
   #   mode = "n";
--- nixvim-config/config/user_commands.nix
+++ nixvim-config/config/user_commands.nix
@@ -1,6 +1,6 @@
 {
   userCommands = {
-    MarkdownFormatTable = {
+    MarkdownTableFormat = {
       command = "<line1>,<line2>!column -t -s '|' -o '|'";
       range = true;
       desc = "Format markdown table in visual selection.";

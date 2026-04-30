06c6699d6710948613cd444f466aab857b641eb4
bileamScheuvens <benedictbileam@gmx.de>
2026-04-30 11:12:16 +0200

    add garnix cache

--- hosts/shared_configuration.nix
+++ hosts/shared_configuration.nix
@@ -137,11 +137,13 @@
       "https://nix-community.cachix.org/"
       "https://hyprland.cachix.org/"
       "https://cache.nixos-cuda.org/"
+      "https://cache.garnix.io"
     ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
     ];
   };
 

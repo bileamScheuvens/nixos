bea17bf82d588e24cfdbe83e250862321003fc0a
bileamScheuvens <benedictbileam@gmx.de>
2026-09-18 11:12:16 +0200

    fira

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -4,6 +4,9 @@
     fontconfig.enable = true;
     packages = with pkgs; [
       font-awesome
+      nerd-fonts.jetbrains-mono
+      fira-code
+      fira-code-symbols
       libertinus
     ];
   };
